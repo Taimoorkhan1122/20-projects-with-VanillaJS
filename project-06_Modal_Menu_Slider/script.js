7033c21ea09ff20dad4e950f353bd94f4dc76fc2
Taimoor khan <khant8k83@gmail.com>
2020-08-27 00:01:12 +0500

    completed Modal slider landing page

--- project-06_Modal_Menu_Slider/script.js
+++ project-06_Modal_Menu_Slider/script.js
@@ -1,1 +1,20 @@
-const 
+const toggle = document.getElementById("toggle");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const modal = document.getElementById("modal");
+
+open.addEventListener("click", () => {
+  modal.classList.add("show-modal");
+});
+
+close.addEventListener("click", () => {
+  modal.classList.remove("show-modal");
+});
+
+window.addEventListener("click", (e) => {
+  e.target === modal ? modal.classList.remove("show-modal") : false;
+});
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("show-nav");
+});
